2c585554d7a693947ba37bb1cec11b8c9d92de38
Alexis Hachemi <alexishachemi@gmail.com>
2025-01-21 18:25:05 +0100

    Update main

--- src/main.c
+++ src/main.c
@@ -11,232 +11,63 @@
 #include "raylib.h"
 #include "world.h"
 #include "render.h"
-// #include "orientation.h"
-// #include "prop.h"
-// #include "v2.h"
-
-// static void setup_debug_map(renderer_t *r, world_t *world)
-// {
-//     texture_t *grass_texture = renderer_new_texture(r, "grass", "tests/assets/test_terrain.png");
-//     texture_t *tree_texture = renderer_new_texture(r, "tree", "tests/assets/test_prop.png");
-//     texture_t *sofa_texture = renderer_new_texture(r, "sofa", "tests/assets/test_sofa.png");
-//     texture_t *bed_texture = renderer_new_texture(r, "bed", "tests/assets/bed.png");
-//     texture_t *painting_texture = renderer_new_texture(r, "painting", "tests/assets/painting.png");
-//     texture_t *floor_texture = renderer_new_texture(r, "floor", "tests/assets/floor.png");
-
-//     asset_t *grass_asset = world_new_asset(world, "grass", grass_texture, (Rectangle){0, 0, 32, 32});
-//     asset_t *tree_asset = world_new_asset(world, "tree", tree_texture, (Rectangle){0, 0, 32, 48});
-//     asset_t *big_tree_asset = world_new_asset(world, "big_tree", tree_texture, (Rectangle){32, 0, 64, 96});
-    
-//     asset_t *left_sofa_down_asset = world_new_asset(world, "l_sofa_d", sofa_texture, (Rectangle){0, 0, 32, 42});
-//     asset_t *right_sofa_down_asset = world_new_asset(world, "r_sofa_d", sofa_texture, (Rectangle){32, 0, 32, 42});
-
-//     asset_t *left_sofa_right_asset = world_new_asset(world, "l_sofa_r", sofa_texture, (Rectangle){0, 42, 32, 44});
-//     asset_t *right_sofa_right_asset = world_new_asset(world, "r_sofa_r", sofa_texture, (Rectangle){0, 86, 32, 32});
-
-//     asset_t *left_sofa_left_asset = world_new_asset(world, "l_sofa_l", sofa_texture, (Rectangle){32, 86, 32, 32});
-//     asset_t *right_sofa_left_asset = world_new_asset(world, "r_sofa_l", sofa_texture, (Rectangle){32, 42, 32, 44});
-    
-
-//     asset_t *tl_bed_asset = world_new_asset(world, "bed", bed_texture, (Rectangle){0, 0, 32, 32});
-//     asset_t *tr_bed_asset = world_new_asset(world, "tr_bed", bed_texture, (Rectangle){32, 0, 32, 32});
-//     asset_t *bl_bed_asset = world_new_asset(world, "bl_bed", bed_texture, (Rectangle){0, 32, 32, 32});
-//     asset_t *br_bed_asset = world_new_asset(world, "br_bed", bed_texture, (Rectangle){32, 32, 32, 32});
-//     asset_t *painting_asset = world_new_asset(world, "painting", painting_texture, (Rectangle){0, 0, 32, 32});
-//     asset_t *floor_asset = world_new_asset(world, "floor", floor_texture, (Rectangle){0, 0, 32, 32});
-
-//     terrain_t *grass_terrain = world_new_terrain(world, "grass", grass_asset);
-//     prop_t *tree_prop = world_new_prop(world, "tree");
-//     prop_t *big_tree_prop = world_new_prop(world, "big_tree");
-//     prop_t *sofa_prop = world_new_prop(world, "sofa");
-//     prop_t *sofa_prop_r = prop_add_child(sofa_prop, (v2_t){1, 0});
-//     prop_t *tl_bed_prop = world_new_prop(world, "bed");
-//     prop_t *tr_bed_prop = prop_add_child(tl_bed_prop, (v2_t){1, 0});
-//     prop_t *bl_bed_prop = prop_add_child(tl_bed_prop, (v2_t){0, 1});
-//     prop_t *br_bed_prop = prop_add_child(tl_bed_prop, (v2_t){1, 1});
-//     prop_t *painting_prop = world_new_prop(world, "painting");
-//     world_new_terrain(world, "floor", floor_asset);
-
-//     prop_set_mono_asset(tree_prop, tree_asset);
-//     prop_set_mono_asset(big_tree_prop, big_tree_asset);
-
-//     prop_set_multi_asset(
-//         sofa_prop,
-//         left_sofa_down_asset,
-//         left_sofa_down_asset,
-//         left_sofa_left_asset,
-//         left_sofa_right_asset
-//     );
-
-//     prop_set_multi_asset(
-//         sofa_prop_r,
-//         right_sofa_down_asset,
-//         right_sofa_down_asset,
-//         right_sofa_left_asset,
-//         right_sofa_right_asset
-//     );
-
-//     prop_set_z_index(big_tree_prop, 1);
-//     prop_set_z_index(tree_prop, 0);
-//     prop_set_mono_asset(tl_bed_prop, tl_bed_asset);
-//     prop_set_mono_asset(tr_bed_prop, tr_bed_asset);
-//     prop_set_mono_asset(bl_bed_prop, bl_bed_asset);
-//     prop_set_mono_asset(br_bed_prop, br_bed_asset);
-//     prop_set_z_index(tl_bed_prop, 0);
-//     prop_set_mono_asset(painting_prop, painting_asset);
-//     prop_set_z_index(painting_prop, 1);
-
-//     // srand(time(NULL));
-//     for (size_t y = 0; y < world->size; y++) {
-//         for (size_t x = 0; x < world->size; x++) {
-//             world_place_terrain(world, (v2_t){x, y}, grass_terrain);
-//             if (rand() % 5 == 0)
-//                 world_place_prop(world, big_tree_prop, (v2_t){x, y}, ORIENT_DOWN, false);
-//             else if (rand() % 7 == 0)
-//                 world_place_prop(world, tree_prop, (v2_t){x, y}, ORIENT_LEFT, false);
-//         }
-//     }
-// }
-
-// static void setup_room(world_t *world)
-// {
-//     const char *name = "interior";
-//     Rectangle bounds = {20, 20, 10, 6};
-
-//     csp_object_t *obj = NULL;
-//     csp_room_t *room = NULL;
-
-//     room = world_new_room(world, name);
-//     room->terrain = world_get_terrain(world, "floor");
-
-//     if (!csp_set_all_cell_connected(room, true)) {
-//         dprintf(2, "Failed to set all cell connected");
-//         return;
-//     }
-
-//     obj = csp_room_add_obj(room);
-//     csp_obj_add_prop(obj, world_get_prop(world, "bed"));
-//     csp_set_on_ground(obj, true);
-//     csp_set_adjacent_to_wall(obj, false);
-//     csp_set_has_orient(obj, true, ORIENT_DOWN);
-//     csp_set_amount(obj, 2);
-
-//     obj = csp_room_add_obj(room);
-//     csp_obj_add_prop(obj, world_get_prop(world, "painting"));
-//     csp_set_has_orient(obj, true, ORIENT_DOWN);
-//     csp_set_on_top_of_prop(obj, true, world_get_prop(world, "bed"));
-//     csp_set_amount(obj, 1);
-
-//     obj = csp_room_add_obj(room);
-//     csp_obj_add_prop(obj, world_get_prop(world, "sofa"));
-//     csp_set_on_ground(obj, true);
-//     csp_set_reserved_space(obj, true, (v2_t){1, 0});
-//     csp_set_adjacent_to_wall(obj, true);
-//     csp_set_amount(obj, 3);
-//     obj->chance = 0.5;
-//     csp_set_adjacent_to_prop(obj, false, world_get_prop(world, "sofa"));
-//     csp_set_has_orient(obj, true, ORIENT_RIGHT);
-//     csp_set_has_orient(obj, true, ORIENT_LEFT);
-//     csp_set_has_orient(obj, true, ORIENT_DOWN);
-
-//     if (!world_generate_room(world, name, bounds))
-//         printf("-- Room generation failed --\n");
-// }
-
-// int MAIN(void)
-// {
-//     world_t world = {0};
-//     renderer_t renderer = {0};
-//     unsigned int seed = time(NULL);
-
-//     srand(seed);
-//     SetRandomSeed(seed);
-//     if (!world_init(&world, 50, 0)) {
-//         dprintf(2, "ERROR: Failed to initialize world\n");
-//         return EXIT_FAILURE;
-//     }
-//     renderer_init(&renderer, &(display_settings_t){
-//         .screen_width=800,
-//         .screen_height=600,
-//         .tile_size_px=32
-//     });
-//     setup_debug_map(&renderer, &world);
-//     setup_room(&world);
-//     render_and_display(&renderer, &world);
-//     renderer_deinit(&renderer);
-//     world_deinit(&world);
-//     return EXIT_SUCCESS;
-// }
-
-#include <stdio.h>
+#include "bsp.h"
+#include "linked.h"
+#include "registry.h"
 #include "parser.h"
-#include "p_color.h"
 
-int MAIN(void)
+static bool place_rooms(world_t *world, bsp_t *bsp)
 {
-    world_t world = {0};
-    renderer_t renderer = {0};
-    unsigned int seed = time(NULL);
-    const char *room = "living_room";
+    list_t leaves = {0};
+    unsigned int idx = 0;
+    unsigned int wrapped_idx = 0;
+    Rectangle bounds = {0};
+    bsp_t *leaf = NULL;
 
-    srand(seed);
-    if (!parse_config(&world, &renderer, "examples/config/interiors.json"))
-        return EXIT_FAILURE;
-    // setup_debug_map(&renderer, &world);
-    if (!world_generate_room(&world, room, (Rectangle){0, 0, 7, 7}))
-        dprintf(2, P_RED "[ERROR]" P_END ": Failed to generate room %s\n", room);
-    render_and_display(&renderer, &world);
-    world_deinit(&world);
-    renderer_deinit(&renderer);
-    return EXIT_SUCCESS;
-}
-
-#include <stdio.h>
-#include "parser.h"
-#include "p_color.h"
-
-int MAIN(void)
-{
-    world_t world = {0};
-    renderer_t renderer = {0};
-    unsigned int seed = time(NULL);
-    const char *room = "living_room";
-
-    srand(seed);
-    if (!parse_config(&world, &renderer, "examples/config/interiors.json"))
-        return EXIT_FAILURE;
-    // setup_debug_map(&renderer, &world);
-    if (!world_generate_room(&world, room, (Rectangle){0, 0, 7, 7}))
-        dprintf(2, P_RED "[ERROR]" P_END ": Failed to generate room %s\n", room);
-    render_and_display(&renderer, &world);
-    world_deinit(&world);
-    renderer_deinit(&renderer);
-    return EXIT_SUCCESS;
+    list_init(&leaves);
+    if (!bsp_get_leaves(bsp, &leaves))
+        return false;
+    for (node_t *n = leaves.head; n; n = n->next, idx++) {
+        leaf = n->data;
+        bounds = (Rectangle){
+            leaf->room.pos.x,
+            leaf->room.pos.y,
+            leaf->room.size.x,
+            leaf->room.size.y
+        };
+        wrapped_idx = idx % world->rooms.last_free_index;
+        if (!world_generate_room(world, REG_AT(csp_room_t, &world->rooms, wrapped_idx)->name, bounds))
+            return false;
+    }
+    list_clear(&leaves, NULL);
+    return true;
 }
 
 int MAIN(void)
 {
     world_t world = {0};
     renderer_t renderer = {0};
+    unsigned int seed = time(NULL);
     bsp_t bsp = {0};
 
-    srand(time(NULL));
-    if (!parse_config(&world, &renderer, "test.json")) {
+    srand(seed);
+    SetRandomSeed(seed);
+    if (!parse_config(&world, &renderer, "examples/config/interiors.json")) {
         dprintf(2, "Failed to parse config\n");
         return EXIT_FAILURE;
     }
-    setup_debug_map(&world);
     if (!bsp_init(&bsp, (bsp_rect_t){{0,0}, {world.size, world.size}})) {
         dprintf(2, "Failed to initialize bsp\n");
         return EXIT_FAILURE;
     }
     bsp.s_settings = (bsp_split_settings_t) {
-        .splits = 3,
+        .splits = world.rooms.last_free_index - 1,
         .max_ratio = 2.7,
         .same_split_percent = 0
     };
+    bsp.s_settings.splits = bsp.s_settings.splits <= 0 ? 1 : bsp.s_settings.splits;
     bsp.r_settings = (bsp_room_settings_t){
-        .max_ratio = 1.3,
+        .max_ratio = 1.0,
         .min_coverage_percent = 10,
         .max_coverage_percent = 20,
         .spacing_scale = 0.5
@@ -249,49 +80,10 @@ int MAIN(void)
         dprintf(2, "Failed to generate BSP\n");
         return EXIT_FAILURE;
     }
-    setup_rooms(&world, &bsp);
-    render_and_display(&renderer, &world);
-    world_deinit(&world);
-    renderer_deinit(&renderer);
-    return EXIT_SUCCESS;
-}
-
-int MAIN(void)
-{
-    world_t world = {0};
-    renderer_t renderer = {0};
-    bsp_t bsp = {0};
-
-    srand(time(NULL));
-    if (!parse_config(&world, &renderer, "test.json")) {
-        dprintf(2, "Failed to parse config\n");
+    if (!place_rooms(&world, &bsp)) {
+        dprintf(2, "Failed to place rooms\n");
         return EXIT_FAILURE;
     }
-    setup_debug_map(&world);
-    if (!bsp_init(&bsp, (bsp_rect_t){{0,0}, {world.size, world.size}})) {
-        dprintf(2, "Failed to initialize bsp\n");
-        return EXIT_FAILURE;
-    }
-    bsp.s_settings = (bsp_split_settings_t) {
-        .splits = 3,
-        .max_ratio = 2.7,
-        .same_split_percent = 0
-    };
-    bsp.r_settings = (bsp_room_settings_t){
-        .max_ratio = 1.3,
-        .min_coverage_percent = 10,
-        .max_coverage_percent = 20,
-        .spacing_scale = 0.5
-    };
-    bsp.c_settings = (bsp_corridor_settings_t){
-        .room_link_min_touch = 10,
-        .cycling_rate = 0.1
-    };
-    if (!bsp_generate(&bsp)) {
-        dprintf(2, "Failed to generate BSP\n");
-        return EXIT_FAILURE;
-    }
-    setup_rooms(&world, &bsp);
     render_and_display(&renderer, &world);
     world_deinit(&world);
     renderer_deinit(&renderer);
