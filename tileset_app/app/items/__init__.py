421a2ac3815faa6b01f2d7107b195e59e671b979
mathematisse <lucien.pineau@epitech.eu>
2025-01-29 05:20:32 +0100

    Edit exported items elements

--- tileset_app/app/items/__init__.py
+++ tileset_app/app/items/__init__.py
@@ -2,18 +2,22 @@
 Module containing all editable items
 """
 
-from .asset import Asset, DirectionEnum
+from .asset import Asset, AssetDict, DirectionEnum, NamedAssetDict, UnnamedMonoAsset
 from .base_item import BaseItem, ItemT
 from .object import RoomObject
-from .prop import Prop
-from .room import Room
+from .prop import Prop, PropDict
+from .room import Room, RoomDict
 
 __all__ = [
     "BaseItem",
     "ItemT",
     "Asset",
+    "AssetDict",
+    "NamedAssetDict",
     "DirectionEnum",
     "Prop",
+    "PropDict",
     "Room",
+    "RoomDict",
     "RoomObject",
 ]
